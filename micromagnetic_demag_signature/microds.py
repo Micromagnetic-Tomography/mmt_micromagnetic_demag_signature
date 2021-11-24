afb00cd4ce9ebf8350fe034fd941414fdb695e67
davidcortesortuno <david.cortes.o@gmail.com>
2021-11-24 17:28:21 +0100

    Documenting main class

--- micromagnetic_demag_signature/microds.py
+++ micromagnetic_demag_signature/microds.py
@@ -12,32 +12,39 @@ nm = 1e-9
 @numba.njit(parallel=True)
 def dipole_Bz(dip_r, dip_m, Sx_range, Sy_range, Sheight, Bz_grid):
     """
-    Compute the z-component of the dipole field at the pos_r position(s), from
-    a group of particles located at the dip_r dipole_positions, and which have
-    magnetic dipole moments given in the dip_m array.
+    Compute the z-component of the dipole field at the node position(s) of a
+    scan grid, from a group of particles located at the dip_r dipole_positions,
+    and which have magnetic dipole moments given in the dip_m array. The scan
+    grid is defined by rectangular sensors whose center are defined by the
+    Sx_range and Sy_range points, so the scan surface is defined by a `Nx x Ny`
+    grid of points.
 
     For these arrays, N > 1
 
     Parameters
     ----------
-
     dip_r
         N x 3 array with dipole positions (m)
     dip_m
         N x 3 array with dipole moments (Am^2)
-    pos_r
-        M X P x 3 array (grid) with coordinates of measurement point (m)
-
+    Sx_range
+        M-array with coordinates of measurement point (m) in the x-direction
+    Sy_range
+        P-array with coordinates of measurement point (m) in the y-direction
+    Sheight
+        Height or z-position of the scan surface
+    Bz_grid
+        M x P array to be populated with the dipole field values
     """
     if Bz_grid.shape[0] * Bz_grid.shape[1] != Sx_range.shape[0] * Sy_range.shape[0]:
         raise Exception('Bz grid array with wrong dimensions')
 
-    # For every row of dip_r (Nx3 array), subtract pos_r (1x3 array)
     pos_r = np.zeros(3)
     pos_r[2] = Sheight
     for j, sy in enumerate(Sy_range):
         for i, sx in enumerate(Sx_range):
             pos_r[0], pos_r[1] = sx, sy
+            # Subtract every row of dip_r (Nx3 array) from pos_r (1x3 array):
             r = pos_r - dip_r
 
             x, y, z = r[:, 0], r[:, 1], r[:, 2]
@@ -67,21 +74,54 @@ class MicroDemagSignature(object):
                  merrill_mag_vbox_file,
                  merrill_energy_log_file=None
                  ):
-        """TODO: to be defined.
-
-        :Docstring for MMTDemagsignal.: TODO
+        """
+        This class allows to calculate the dipolar signal from the
+        magnetization vectors of a finite element micromagnetic system
+        simulated with the MERRILL code. The dipolar signal is recorded in a
+        rectangular scan grid that is defined when instatiating this class.
+        Information of the micromagnetic model is defined in a vbox file
+        produced by MERRILL.
 
         Parameters
         ----------
-
+        scan_limits
+            A sequence containing 2 pairs of floating values, defining the scan
+            limits, e.g. [[0, 0], [1e-6, 1e-6]]. These points are the centers
+            of the sensors in the west-south corner and the north-east corner
+            respectively (see Notes). Specified in meters
+        scan_spacing
+            A sequence with 2 values for the scan grid spacing in the x and y
+            directions (see Notes). Specified in meters
+        scan_height
+            Position of the scan grid in the z-direction. Specified in meters
         merrill_mag_vbox_file
-            Path to the MERRILL output file with 7 columns:
+            Path to a MERRILL's vbox output file with 7 columns:
                 x y z mx my mz volume
             where the spatial data is scaled in µm
         merrill_energy_log_file (optional)
             Path to the MERRILL energy file from which magnetic paramters are
-            read from the header
+            read in the header
 
+        Notes
+        -----
+        For a scan grid the scan limit pairs define its corners as:
+
+                -----------------
+                |   |   |   | o | -- scan_limits[1]
+                -----------------
+                |   |   |   |   |
+                -----------------
+                | o |   |   |   |
+                -----------------
+                  |
+              scan_limits[0]
+
+        By specifying a scan_spacing these points are included. For instance,
+        using `scan_spacing=[[0, 0], [1e-6, 1e-6]]` and
+        `scan_spacing=[0.05e-6, 0.05e-6]` the grid has 21 points in each
+        direction. If `scan_spacing` is not dividing the limits exactly by an
+        integer number, then the scan grid limits are approximated. Check the
+        `Sx_range` and `Sy_range` variables to check this.
         """
 
         self.scan_limits = scan_limits
@@ -103,7 +143,9 @@ class MicroDemagSignature(object):
 
     def _read_magnetic_params(self, log_file):
         """
-        Just assigning Ms for now
+        Reads the saturation magnetization value from the log file of a MERRILL
+        simulation.
+        TODO: Might require to check MERRILL keeps the log file format intact
         """
         line = ''
         with open(log_file, 'r') as f:
@@ -118,7 +160,7 @@ class MicroDemagSignature(object):
         # print('Mat params:', self.material_parameters)
         self.Ms = mat_params[1]
 
-    def read_input_files(self, 
+    def read_input_files(self,
                          Ms=None,
                          origin_to_geom_center=False):
         """
@@ -144,6 +186,7 @@ class MicroDemagSignature(object):
                              'setting the Ms argument or via a log file in '
                              'the constructor')
 
+        # Read the vbox file: TODO: we can use a faster method for large files
         self.mag_data = np.loadtxt(self.mag_vbox_file, skiprows=1, ndmin=2)
         # Scale spatial data:
         self.mag_data[:, :3] *= µm
@@ -174,6 +217,14 @@ class MicroDemagSignature(object):
 
     def compute_scan_signal(self, method='numba'):
         """
+        Computes the dipolar signal at the scan surface
+
+        Parameters
+        ----------
+        method
+            Specify `numba` or `cython` for the calculations. The C method is
+            parallelized with OpenMP. Results are saved in the `self.Bz_grid`
+            array.
         """
         # print(self.dip_moments.shape)
         if method == 'numba':
